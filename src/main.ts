8d4dd647f4d0b333be95f84e19b4787b3a244934
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2026-01-29 21:08:46 +0500

    Filters

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { logger } from './common/middlewares/logger.middleware';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
+import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -10,6 +11,9 @@ async function bootstrap() {
     app.useGlobalPipes(new ValidationPipe());
 
     app.useGlobalInterceptors(new ResponseInterceptor());
+
+    app.useGlobalFilters(new AllExceptionsFilter());
+
     app.use(logger);
 
     await app.listen(3000);
