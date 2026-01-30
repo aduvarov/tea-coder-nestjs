33eb5c8ff04ad5f3ebbaf68282e040b0e2011906
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2026-01-30 18:07:58 +0500

    Practice init...

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MovieModule } from './movie/movie.module';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 
@@ -11,7 +10,6 @@ import { PrismaModule } from './prisma/prisma.module';
             isGlobal: true,
         }),
         PrismaModule,
-        MovieModule,
     ],
     controllers: [AppController],
     providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { logger } from './common/middlewares/logger.middleware';
-import { ResponseInterceptor } from './common/interceptors/response.interceptor';
-import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { MovieModule } from './movie/movie.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-
-    app.useGlobalPipes(new ValidationPipe());
-
-    app.useGlobalInterceptors(new ResponseInterceptor());
-
-    app.useGlobalFilters(new AllExceptionsFilter());
 
     const config = new DocumentBuilder()
         .setTitle('Nest Cource API')
@@ -28,17 +17,13 @@ async function bootstrap() {
         .addBearerAuth()
         .build();
 
-    const document = SwaggerModule.createDocument(app, config, {
-        include: [MovieModule],
-    });
+    const document = SwaggerModule.createDocument(app, config);
 
     SwaggerModule.setup('/docs', app, document, {
         jsonDocumentUrl: '/swagger.json',
         yamlDocumentUrl: '/yaml.json',
         customSiteTitle: 'Nest js Api docs',
     });
-
-    app.use(logger);
 
     await app.listen(3000);
 }
