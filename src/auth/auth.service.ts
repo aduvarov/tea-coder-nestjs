9ae383028b9c649019661db5e75f39c51c6f7b95
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2026-01-30 20:48:07 +0500

    Project1. Установка JWT. Создание access и refresh токенов

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,10 +2,26 @@ import { ConflictException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { RegisterRequest } from './dto/register.dto';
 import { hash } from 'argon2';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './interfaces/jwt.interface';
+import type { StringValue } from 'ms';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly prismaService: PrismaService) {}
+    private readonly JWT_SECRET: string;
+    private readonly JWT_ACCESS_TOKEN_TTL: string;
+    private readonly JWT_REFRESH_TOKEN_TTL: string;
+
+    constructor(
+        private readonly prismaService: PrismaService,
+        private readonly configService: ConfigService,
+        private readonly jwtService: JwtService,
+    ) {
+        this.JWT_SECRET = configService.getOrThrow<string>('JWT_SECRET');
+        this.JWT_ACCESS_TOKEN_TTL = configService.getOrThrow<string>('JWT_ACCESS_TOKEN_TTL');
+        this.JWT_REFRESH_TOKEN_TTL = configService.getOrThrow<string>('JWT_REFRESH_TOKEN_TTL');
+    }
 
     async register(dto: RegisterRequest) {
         const { name, email, password } = dto;
@@ -17,9 +33,7 @@ export class AuthService {
         });
 
         if (existUser) {
-            throw new ConflictException(
-                'Пользователь с такой почтой уже существует',
-            );
+            throw new ConflictException('Пользователь с такой почтой уже существует');
         }
 
         const user = await this.prismaService.user.create({
@@ -30,6 +44,20 @@ export class AuthService {
             },
         });
 
-        return user;
+        return this.generateTokens(user.id);
+    }
+
+    private generateTokens(id: string) {
+        const payload: JwtPayload = { id };
+
+        const accessToken = this.jwtService.sign(payload, {
+            expiresIn: this.JWT_ACCESS_TOKEN_TTL as StringValue,
+        });
+
+        const refreshToken = this.jwtService.sign(payload, {
+            expiresIn: this.JWT_REFRESH_TOKEN_TTL as StringValue,
+        });
+
+        return { accessToken, refreshToken };
     }
 }
