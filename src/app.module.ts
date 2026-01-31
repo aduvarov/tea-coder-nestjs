1e871ad59fced13db9b639559983d8d2c78d1389
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2026-01-31 23:42:37 +0500

    GraphQL init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { getGraphQLConfig } from './config/graphql.config';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             isGlobal: true,
         }),
+        GraphQLModule.forRootAsync<ApolloDriverConfig>({
+            driver: ApolloDriver,
+            useFactory: getGraphQLConfig,
+        }),
         PrismaModule,
         AuthModule,
+        UserModule,
     ],
-    controllers: [AppController],
-    providers: [AppService],
 })
 export class AppModule {}
